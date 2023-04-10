38f3f1463b96835e0f3677ef751ebdae157e9ca0
NamasakaLennox <namasakalennox254@gmail.com>
2023-04-10 12:22:12 +0300

    [new]: a simple unicafe rating plain site

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,42 +6,79 @@ const Button = (props) => (
   <button onClick={props.handleClick}>{props.text}</button>
 );
 
-const Stats = (props) => (
-  <>
-    <h1>statistics</h1>
-    <p>
-      good {props.good} <br></br>
-      neutral {props.neutral} <br></br>
-      bad {props.bad}
-    </p>
-  </>
+const StatisticLine = ({ text, value, sign }) => (
+  <tbody>
+    <tr>
+      <td>{text}</td>
+      <td>
+        {value} {sign ? "%" : ""}
+      </td>
+    </tr>
+  </tbody>
 );
+const Statistics = (props) => {
+  const good = props.stats[0];
+  const neutral = props.stats[1];
+  const bad = props.stats[2];
+  const clicks = props.stats[3];
+  if (clicks === 0) {
+    return (
+      <>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </>
+    );
+  }
+  return (
+    <>
+      <h1>statistics</h1>
+      <table>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={clicks} />
+        <StatisticLine text="average" value={(good * 1 + bad * -1) / clicks} />
+        <StatisticLine
+          text="positive"
+          value={(good / clicks) * 100}
+          sign={true}
+        />
+      </table>
+    </>
+  );
+};
 
 const App = () => {
   // save clicks of each button to its own line
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [clicks, setClicks] = useState(0);
 
   const handleGoodClick = () => {
     const goodTotal = good + 1;
+    setClicks(clicks + 1);
     setGood(goodTotal);
   };
   const handleBadClick = () => {
     const badTotal = bad + 1;
+    setClicks(clicks + 1);
     setBad(badTotal);
   };
   const handleNeutralClick = () => {
     const neutralTotal = neutral + 1;
+    setClicks(clicks + 1);
     setNeutral(neutralTotal);
   };
+  const stats = [good, neutral, bad, clicks];
+
   return (
     <div>
       <Header />
       <Button handleClick={handleGoodClick} text="good" />
       <Button handleClick={handleNeutralClick} text="neutral" />
       <Button handleClick={handleBadClick} text="bad" />
-      <Stats good={good} neutral={neutral} bad={bad} />
+      <Statistics stats={stats} />
     </div>
   );
 };
