64900afc1a0c2cae2e2d2195570506a627dad91d
NamasakaLennox <namasakalennox254@gmail.com>
2023-05-01 17:19:25 +0300

    [update]: added fetching data from json server functionality

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,21 +1,26 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [pageFilter, setFilter] = useState(false);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newFilter, setNewFilter] = useState("");
+
+  useEffect(() => {
+    //console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      // console.log("promise fulfilled");
+      setPersons(response.data);
+    });
+  }, []);
+  console.log(">", persons.length, "rendered");
 
   const filterPage = (event) => {
     event.preventDefault();
